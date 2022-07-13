fe7951ed51e3d747269d590d286a6db5905b1438
DavideManciucca <davidemanciucca@gmail.com>
2022-07-13 17:29:57 +0200

    importato nella home del front il componente vue

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/front/style.css')}}">
+
     <title>prova</title>
 </head>
 <body>
     prova home guest
+    <div id='app'></div>
+    <script src="{{asset('js/front.js')}}"></script>
 </body>
 </html>
